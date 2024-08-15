3762e76318a2e0f0a106288dc7f388b452725f90
perfer95 <perfer95@outlook.com>
2024-08-15 06:52:44 +0000

    7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 """
 from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Takes a string k and an int OR float v as
     arguments and returns a tuple.
