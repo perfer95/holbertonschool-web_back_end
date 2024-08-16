2f74fece38a3a32cb909abd840a9eb9664cac310
perfer95 <perfer95@outlook.com>
2024-08-16 23:48:19 +0000

    1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     return the list of all the delays (float values)
     in ascending order.
