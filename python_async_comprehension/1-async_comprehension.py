a08b2110d7cb9fa758cf25654967a8b3903da88c
perfer95 <perfer95@outlook.com>
2024-08-21 09:55:21 +0000

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,8 +3,9 @@
 1. Async Comprehensions
 """
 
-from type import List
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
