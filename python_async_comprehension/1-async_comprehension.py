93b08f9a97fe8e566f70943c02791fd593d1e969
perfer95 <perfer95@outlook.com>
2024-08-21 10:00:12 +0000

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,7 @@
 1. Async Comprehensions
 """
 
+import asyncio
 from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
@@ -12,5 +13,7 @@ async def async_comprehension() -> List[float]:
     Collect 10 random numbers using an async comprehensing over
     async_generator, then return the 10 random numbers.
     """
-    return [numbers async for value in async_generator()]
+    return [numbers async for numbers in async_generator()]
 
+if __name__ == "__main__":
+    asyncio.run(async_comprehension())
