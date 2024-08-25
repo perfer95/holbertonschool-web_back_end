86dd64debe2d2784d8da4331235fdc17c5e0732a
perfer95 <perfer95@outlook.com>
2024-08-25 12:15:17 +0000

    8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,7 +4,7 @@ import pymongo
 
 
 def list_all(mongo_collection):
-    """ lists all documents in a collection """
+    """ Test"""
     if not mongo_collection:
         return []
     return list(mongo_collection.find())
