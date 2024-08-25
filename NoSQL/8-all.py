87c0f988227a899445caa26470f9eb5e4f37240e
perfer95 <perfer95@outlook.com>
2024-08-25 11:46:12 +0000

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+8. List all documents in Python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    Considering that pymongo was imported
+    """
+    documents = mongo_collection.find()
+    if documents.count() == 0:
+        return []
+    return documents
