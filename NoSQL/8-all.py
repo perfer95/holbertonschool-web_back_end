2f28a7b85f40331caa6e81f6ab9656546d1ebc20
perfer95 <perfer95@outlook.com>
2024-09-08 20:46:06 -0600

    8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-"""
-8. List all documents in Python
-"""
-import pymongo
+"""lists all documents in a collection"""
 
 
 def list_all(mongo_collection):
-    """
-    Python function that lists all documents in a collection
-    """
-    docs = mongo_collection.find()
-    if docs.count() == 0:
+    """list all docs"""
+    if mongo_collection is None:
         return []
-    return docs
-
-if __name__ == "__main__":
-    pass
+    return mongo_collection.find()
